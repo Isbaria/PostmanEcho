305561153190659d353b1e4063b738592df9b95e
Alexandr <isbarius@yandex.ru>
2024-03-03 17:18:09 +0300

    Failed

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,23 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnSendData() {
+        var text = "Hi";
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("hi")
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", org.hamcrest.Matchers.equalTo(text))
+        ;
+    }
+}
